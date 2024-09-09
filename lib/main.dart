98a89544a77b001225a59e321e68d5db7d9aea5c
Thomaz Alves <thomazhilario5@gmail.com>
2024-09-09 13:29:16 -0300

    Feat(PAge Destinos): Criar a pagina de destinos
    - Criar a pagina de destinos.
    - Criar um componente que exibe informações sobre cada destino.

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Home Page
 class MyHomePage extends StatelessWidget {
   final String title;
 
@@ -79,15 +80,17 @@ class MyHomePage extends StatelessWidget {
           width: 300,
           height: 50,
           child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromARGB(138, 104, 68, 223),
-            ),
-            onPressed: () {
-            Navigator.pop(context);
-            Navigator.pushNamed(context, '/destinos');
-          },
-          child: const Text('Explorar destinos ', style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),)),
-          ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(138, 104, 68, 223),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/destinos');
+              },
+              child: const Text(
+                'Explorar destinos ',
+                style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+              )),
+        ),
       ),
 
       // This trailing comma makes auto-formatting nicer for build methods.
@@ -95,26 +98,48 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+// Destinos Page
 class Destinos extends StatelessWidget {
   // Constructor
   const Destinos({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Mostar detalhes
+    mostrarDetalhes(String url, String titleDestino, String sobre) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (BuildContext context) =>
+                DetalhesDestinosPage(url: url,sobre: sobre,titleDestino: titleDestino,),
+          ));
+    }
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Second Page')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Back to Home Page'),
+      appBar: AppBar(title: const Text('Destinos')),
+      body: ListView(children: <ListTile>[
+        ListTile(
+          leading: const CircleAvatar(
+            backgroundImage: AssetImage('images/londres.jpg'),
+          ),
+          title: const Text('Londres'),
+          onTap: () => mostrarDetalhes('images/londres.jpg', 'Londres',
+              'Londres, capital da Inglaterra e do Reino Unido, é uma cidade do século 21 com uma história que remonta à era romana. Seu centro abriga as sedes imponentes do Parlamento, a famosa torre do relógio do Big Ben e a Abadia de Westminster, local de coroação dos monarcas britânicos. Do outro lado do rio Tâmisa, a roda gigante London Eye tem vista panorâmica do complexo cultural da margem sul e de toda a cidade.'),
         ),
-      ),
+        ListTile(
+          leading: const CircleAvatar(
+            backgroundImage: AssetImage('images/odaiba.jpg'),
+          ),
+          title: const Text('Odaiba'),
+          onTap: () => mostrarDetalhes('images/odaiba.jpg', 'Odaiba',
+              'Acessado pela Rainbow Bridge ou pelo trem Yurikamome futurista, Odaiba é um centro de entretenimento de alta tecnologia em uma ilha artificial na Baía de Tóquio. Os visitantes vão até a praia no Parque Seaside, aproveitam as vistas do Monte Fuji pela Roda-gigante Daikanransha e interagem com robôs no museu de ciências Miraikan. Os shopping centers incluem o Aqua City e o VenusFort, com temática de Veneza, e há bares de sushi com vista para a orla.'),
+        )
+      ]),
     );
   }
 }
 
+// Contatos Page
 class Contatos extends StatelessWidget {
   const Contatos({super.key});
 
@@ -157,6 +182,30 @@ class Contatos extends StatelessWidget {
               ]),
             ),
           ])),
+    );
+  }
+}
+
+class DetalhesDestinosPage extends StatelessWidget {
+  final String url;
+  final String titleDestino;
+  final String sobre;
+
+  const DetalhesDestinosPage(
+      {super.key,
+      required this.url,
+      required this.titleDestino,
+      required this.sobre});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titleDestino),
+      ),
+      body: Center(
+        child: Text(sobre),
+      ),
     );
   }
 }
